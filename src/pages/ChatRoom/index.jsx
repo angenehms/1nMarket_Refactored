760c2452df465f6d5af42b591b5692e4762ba293
angenehms <angenehms@gmail.com>
2023-12-18 21:12:32 +0900

    [feat] 서버와 채팅방 웹소켓 연결 및 소켓으로 채팅룸 생성, 채팅메세지 발신수신 기능구현(로컬서버 구축병행)

--- src/pages/ChatRoom/index.jsx
+++ src/pages/ChatRoom/index.jsx
@@ -1,10 +1,31 @@
 import { ChatHeader } from 'components';
 import React, { useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 import * as S from './style';
 import { ReactComponent as ChatIcon } from './../../assets/icons/icon-message-circle.svg';
+import { useEffect } from 'react';
+import io from 'socket.io-client';
 
 const ChatRoom = () => {
+
+ // 서버의 주소를 입력합니다.
+ const socket = io('http://localhost:8080');
+ const { id } = useParams()
+
+  useEffect(() => {
+
+    // 서버로부터의 메시지 수신
+    socket.on('serverMsg', (data) => {
+      console.log(data);
+    });
+
+    socket.emit('ask-join', `${id}`);
+
+    // 컴포넌트가 언마운트될 때 소켓 연결을 해제합니다.
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   const [inputValue, setInputValue] = useState('');
   const [searchParams] = useSearchParams();
@@ -14,7 +35,10 @@ const ChatRoom = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // 서버로 메시지 전송
+    socket.emit('clientMsg', {msg:`${inputValue}`, roomId:`${id}`});
     setInputValue('');
+    
   };
 
   const onChangeValue = (e) => {
@@ -45,6 +69,7 @@ const ChatRoom = () => {
         </S.SendBtn>
       </S.FormWrapper>
     </>
+    
   );
 };
 

938d35404e6a471600e25e37ed56761bf939f50a
angenehms <angenehms@gmail.com>
2024-01-31 17:37:13 +0900

    chore : react-cookie 설치 및 폴더, 파일 세팅

--- src/index.js
+++ src/index.js
@@ -7,19 +7,22 @@ import theme from './styles/theme';
 import { store } from './store';
 import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import {CookiesProvider} from 'react-cookie';
 
 const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyled />
-        <App />
-      </ThemeProvider>
-    </Provider>
-  </QueryClientProvider>
+  <CookiesProvider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyled />
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </QueryClientProvider>
+  </CookiesProvider>
   // </React.StrictMode>
 );
